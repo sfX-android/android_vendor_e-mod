b74674006b337c3af9c0fb8a2858f046372a4bf4
steadfasterX <steadfasterX@gmail.com>
2020-07-31 20:30:21 +0200

    noEOSappstore for removing /e/ store
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+# override /e/ apps store
+include $(CLEAR_VARS)
+LOCAL_MODULE := noEOSappstore
+LOCAL_SRC_FILES := empty.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_OVERRIDES_PACKAGES := Apps
+include $(BUILD_PREBUILT)
