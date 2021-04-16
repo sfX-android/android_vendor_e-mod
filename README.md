a76ca79502b586ba2b233c2afb1dec5425713db6
steadfasterX <steadfasterX@gmail.com>
2021-04-16 10:10:41 +0200

    deprecated! pls switch to:
    https://github.com/sfX-android/android_vendor_extendrom

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# android_vendor_e-mod
+# DEPRECATED
+
+switch to: https://github.com/sfX-android/android_vendor_extendrom
+
+#### android_vendor_e-mod
 modifying / extending /e/ OS without touching the regular sources
--- config/common.mk
+++
@@ -1,8 +0,0 @@
-# set e overlay's
-#PRODUCT_PACKAGE_OVERLAYS += vendor/sfx/overlay
-
-#PRODUCT_PACKAGES += \
-#    noEOSappstore
-
-#PRODUCT_COPY_FILES += \
-#    $(PRODUCT_OUT)/system/app/AuroraStore/AuroraStore.apk:system/app/Apps/Apps.apk
